04da6d528080c23060576932dcdefa6814930554
Austin Wise <AustinWise@gmail.com>
2022-02-27 10:35:26 -0800

    Add ASP.NET API client for GRPC service.

---
+++ MyWebApi/Controllers/DoubleGreeterController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyGrpcService;
+
+namespace MyWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DoubleGreeterController : ControllerBase
+    {
+
+        private readonly ILogger<DoubleGreeterController> _logger;
+        private readonly Greeter.GreeterClient _grpcClient;
+
+        public DoubleGreeterController(ILogger<DoubleGreeterController> logger, Greeter.GreeterClient grpClient)
+        {
+            _logger = logger;
+            _grpcClient = grpClient;
+        }
+
+        [HttpGet]
+        public async Task<DoubleGreeting> Get(string a, string b)
+        {
+            var greetA = _grpcClient.SayHelloAsync(new HelloRequest() { Name = a });
+            var greetB = _grpcClient.SayHelloAsync(new HelloRequest() { Name = b });
+            await Task.WhenAll(greetA.ResponseAsync, greetB.ResponseAsync);
+            return new DoubleGreeting()
+            {
+                GreetingA = (await greetA).Message,
+                GreetingB = (await greetB).Message,
+            };
+        }
+    }
+}
---
+++ MyWebApi/DoubleGreeting.cs
@@ -0,0 +1,8 @@
+﻿namespace MyWebApi
+{
+    public class DoubleGreeting
+    {
+        public string? GreetingA { get; set; }
+        public string? GreetingB { get; set; }
+    }
+}
